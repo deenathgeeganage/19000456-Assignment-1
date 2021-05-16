ebaca835ccf91aef5d5705d00d97c359224fa2b8
deenathgeeganage <deenathgeeganage@gmail.com>
2021-05-16 21:02:25 +0530

    Assignment 3rd shipping issue fixed

--- src/Q3_booksale.scala
+++ src/Q3_booksale.scala
@@ -4,8 +4,8 @@ object Q3_booksale extends App {
   def discount(x:Double):Double = (x*40)/100
 
   def shipping(y:Int):Double = {
-    if(y>50) (3+ (y-50)*0.75)
-    else 3
+    if(y>50) ((3*50)+ (y-50)*0.75)
+    else 3*y
   }
 
   def totalcost(a:Int):Double = {
